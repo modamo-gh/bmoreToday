f7dbb967211bf875347bb7760f2c6851d7499ea9
modamo-gh <morganthemosaic@gmail.com>
2025-01-18 14:39:37 -0500

    Set up basic site structure

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import { configDotenv } from "dotenv";
+
+configDotenv();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.send("API o7!");
+});
+
+app.listen(port, () => {
+	console.log(`Server running on port ${port}`);
+});
