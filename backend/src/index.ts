56cd70c13122a5fbdd334194e4f3ceffb5ca9da9
modamo-gh <morganthemosaic@gmail.com>
2025-01-18 21:33:24 -0500

    Connect backend and frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { configDotenv } from "dotenv";
 import router from "./api/events";
 import pool from "../db";
+import cors from "cors";
 
 configDotenv();
 
@@ -9,20 +10,21 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors({origin: "https://bmoretoday.modamo.xyz/"}));
 
-app.use("/api/events", router)
+app.use("/api/events", router);
 
 app.get("/", (req, res) => {
 	res.send("API o7!");
 });
 
 (async () => {
-    try {
-        await pool.query("SELECT NOW()");
-        console.log("Database connected successfully!")
-    } catch (error) {
-        console.error("Error connecting to databases:", error)
-    }
+	try {
+		await pool.query("SELECT NOW()");
+		console.log("Database connected successfully!");
+	} catch (error) {
+		console.error("Error connecting to databases:", error);
+	}
 })();
 
 app.listen(port, () => {
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [events, setEvents] = useState([]);
+
+	useEffect(() => {
+		const fetchEvents = async () => {
+			try {
+				const response = await fetch(
+					"https://bmoretoday.modamo.xyz/api/events"
+				);
+				const data = await response.json();
+
+				setEvents(data);
+			} catch (error) {
+				console.error("Error fetching events", error);
+			}
+		};
+
+		fetchEvents();
+	}, []);
+
+	return (
+		<div>
+			<h1>Bmore Today</h1>
+			<h2>Bmore By Doing More</h2>
+			<p>{JSON.stringify(events)}</p>
+		</div>
+	);
+};
 
 export default App;
