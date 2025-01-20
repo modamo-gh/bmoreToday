7a87b72ad1c0736f9241dc17d3730bfecfd65be2
modamo-gh <morganthemosaic@gmail.com>
2025-01-20 09:54:45 -0500

    Remove manual trigger

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -36,17 +36,6 @@ app.listen(port, () => {
 	console.log(`Server running on port ${port}`);
 });
 
-(async () => {
-    console.log("Manually triggering the cron job...");
-    try {
-        await pool.query("TRUNCATE TABLE events");
-        await getBaltShowPlaceEvents();
-        console.log("Manual run successful");
-    } catch (error) {
-        console.error("Error during manual run:", error);
-    }
-})();
-
 cron.schedule("14 0 * * *", async () => {
 	try {
 		await pool.query("TRUNCATE TABLE events");
