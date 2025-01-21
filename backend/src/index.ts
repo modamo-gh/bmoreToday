94389c39c0a21717cc2f7e1e6e487d4266a13288
modamo-gh <morganthemosaic@gmail.com>
2025-01-20 20:09:26 -0500

    Readd port listen

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import path from "path";
 import router from "./api/events";
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(
@@ -14,4 +15,8 @@ app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req: Request, res: Response) => {
 	res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
+});
+
+app.listen(port, () => {
+	console.log(`Server running on port ${port}`);
 });
