3a8d72db03c907cc3bb41351a7755f7fdc1be4f6
modamo-gh <morganthemosaic@gmail.com>
2025-03-02 12:41:50 -0500

    Use absolute path instead of relative frontend path

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,5 @@
 import cors from "cors";
 import express, { Request, Response } from "express";
-import path from "path";
 import router from "./api/events";
 
 const app = express();
@@ -11,10 +10,10 @@ app.use(
 	cors({ origin: "https://bmoretoday.modamo.xyz", optionsSuccessStatus: 200 })
 );
 app.use("/api/events", router);
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static("/home/modamo/bmoreToday/frontend/build"));
 
 app.get("*", (req: Request, res: Response) => {
-	res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
+	res.sendFile("/home/modamo/bmoreToday/frontend/build/index.html");
 });
 
 app.listen(port, () => {
