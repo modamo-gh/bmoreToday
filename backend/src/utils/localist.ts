d1482ecfac45ae3de748fb4dc19506be1a0e5b11
modamo-gh <morganthemosaic@gmail.com>
2025-03-05 11:57:57 -0500

    Update script to grab all pages, not just the first

--- backend/src/utils/localist.ts
+++ backend/src/utils/localist.ts
@@ -2,39 +2,48 @@ import { DateTime } from "luxon";
 import pool from "../../db";
 
 export const getLocalistEvents = async (baseURL: string) => {
-	const response = await fetch(`${baseURL}/api/2/events`);
-	const data = await response.json();
+	let page = 1;
+	let total = 1;
 
-	const events = data.events;
+	while (page <= total) {
+		const response = await fetch(`${baseURL}/api/2/events?page=${page}`);
+		const data = await response.json();
 
-	for (const event of events) {
-		const title = event.event.title;
-		const location =
-			event.event.experience === "inperson"
-				? event.event.location || event.event.location_name
-				: "Virtual Event";
+		total = data.page.total;
 
-		const startTime = event.event.event_instances[0].event_instance.start;
-		const endTime =
-			event.event.event_instances[0].event_instance.end || startTime;
+		const events = data.events;
 
-		const startDateTime = DateTime.fromISO(startTime);
-		const endDateTime = DateTime.fromISO(endTime);
+		for (const event of events) {
+			const title = event.event.title;
+			const location =
+				event.event.experience === "inperson"
+					? event.event.location || event.event.location_name
+					: "Virtual Event";
 
-		const formattedStartTime = startDateTime.toFormat("hh:mm a");
-		const formattedEndTime = endDateTime.toFormat("hh:mm a");
+			const startTime = event.event.event_instances[0].event_instance.start;
+			const endTime =
+				event.event.event_instances[0].event_instance.end || startTime;
 
-		const time = `${formattedStartTime} - ${formattedEndTime}`;
+			const startDateTime = DateTime.fromISO(startTime);
+			const endDateTime = DateTime.fromISO(endTime);
 
-		const price = event.event.ticket_cost || event.event.filters.event_cost?.[0].name || "Not Provided";
+			const formattedStartTime = startDateTime.toFormat("hh:mm a");
+			const formattedEndTime = endDateTime.toFormat("hh:mm a");
 
-		const url = event.event.localist_url;
+			const time = `${formattedStartTime} - ${formattedEndTime}`;
 
-		const imageURL = event.event.photo_url;
+			const price = event.event.ticket_cost || event.event.filters.event_cost?.[0].name || "Not Provided";
 
-		await pool.query(
-			"INSERT INTO events (title, location, time, price, source, imageURL) VALUES ($1, $2, $3, $4, $5, $6)",
-			[title, location, time, price, url, imageURL]
-		);
+			const url = event.event.localist_url;
+
+			const imageURL = event.event.photo_url;
+
+			await pool.query(
+				"INSERT INTO events (title, location, time, price, source, imageURL) VALUES ($1, $2, $3, $4, $5, $6)",
+				[title, location, time, price, url, imageURL]
+			);
+		}
+
+		page++;
 	}
 };
