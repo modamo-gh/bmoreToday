c27c1e9de8ce9bc192c9150a6ddbbcef29b70152
modamo-gh <morganthemosaic@gmail.com>
2025-03-10 11:52:23 -0400

    Change sidebar to settings with sections

--- frontend/src/components/SourcesSidebar.tsx
+++ frontend/src/components/SourcesSidebar.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { useEventContext } from "../contexts/EventContext";
 import { SourcesSidebarProps } from "../types/SourcesSidebarProps";
 import SourceFilterCheckbox from "./SourceFilterCheckbox";
@@ -15,14 +15,54 @@ const SourcesSidebar: FC<SourcesSidebarProps> = ({ width }) => {
 		setIsEnochPrattLibraryChecked
 	} = useEventContext();
 
+	const [expandedSection, setExpandedSection] = useState("");
+
 	return (
-		<div className="bg-[#232130] flex flex-row lg:flex-col lg:max-h-full lg:w-1/5 items-center rounded-lg w-full">
+		<div className="bg-[#232130] flex flex-row gap-4 lg:flex-col lg:max-h-full lg:w-1/5 items-center p-4 rounded-lg w-full">
 			{width >= 768 && (
 				<h2 className="text-center text-[#f5f5f5] text-lg p-4">
-					Filter Sources
+					Settings
 				</h2>
 			)}
 			<div
+				className={`bg-[#1c1a29] cursor-pointer flex ${
+					expandedSection === "Filter" ? "flex-[8]" : "flex-1"
+				} items-center justify-center rounded-lg w-full`}
+				onClick={() =>
+					setExpandedSection((prev) =>
+						prev !== "Filter" ? "Filter" : ""
+					)
+				}
+			>
+				<h3 className="text-center text-[#f5f5f5] text-lg">Filter</h3>
+			</div>
+			<div
+				className={`bg-[#1c1a29] cursor-pointer flex ${
+					expandedSection === "Sort" ? "flex-[8]" : "flex-1"
+				} items-center justify-center rounded-lg w-full`}
+				onClick={() =>
+					setExpandedSection((prev) =>
+						prev !== "Sort" ? "Sort" : ""
+					)
+				}
+			>
+				<h3 className="text-center text-[#f5f5f5] text-lg">Sort</h3>
+			</div>
+			<div
+				className={`bg-[#1c1a29] cursor-pointer flex ${
+					expandedSection === "Preferences" ? "flex-[8]" : "flex-1"
+				} items-center justify-center rounded-lg w-full`}
+				onClick={() =>
+					setExpandedSection((prev) =>
+						prev !== "Preferences" ? "Preferences" : ""
+					)
+				}
+			>
+				<h3 className="text-center text-[#f5f5f5] text-lg">
+					Preferences
+				</h3>
+			</div>
+			{/* <div
 				className={`flex flex-row gap-2 h-full md:gap-0 lg:flex-col lg:overflow-x-hidden overflow-x-scroll whitespace-nowrap`}
 			>
 				<SourceFilterCheckbox
@@ -68,7 +108,7 @@ const SourcesSidebar: FC<SourcesSidebarProps> = ({ width }) => {
 					}}
 					width={width}
 				/>
-			</div>
+			</div> */}
 		</div>
 	);
 };
