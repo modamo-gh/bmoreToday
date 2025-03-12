369e69b3a4b5b3d4865d68099bef33efcf0355dc
modamo-gh <morganthemosaic@gmail.com>
2025-03-12 14:37:32 -0400

    Wrap app in PreferencesProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { EventProvider } from "./contexts/EventContext.tsx";
+import { PreferencesProvider } from "./contexts/PreferencesContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<EventProvider>
-			<App />
+			<PreferencesProvider>
+				<App />
+			</PreferencesProvider>
 		</EventProvider>
 	</StrictMode>
 );
