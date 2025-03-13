98428f056ac43756b8bd962c857ac9ae49b8b55b
modamo-gh <morganthemosaic@gmail.com>
2025-03-13 18:54:15 -0400

    Modify localist script to send start and end times to db

--- backend/src/utils/localist.ts
+++ backend/src/utils/localist.ts
@@ -20,27 +20,41 @@ export const getLocalistEvents = async (baseURL: string) => {
 					? event.event.location || event.event.location_name
 					: "Virtual Event";
 
-			const startTime = event.event.event_instances[0].event_instance.start;
+			const startTime =
+				event.event.event_instances[0].event_instance.start;
 			const endTime =
-				event.event.event_instances[0].event_instance.end || startTime;
+				event.event.event_instances[0].event_instance.end || null;
 
-			const startDateTime = DateTime.fromISO(startTime, { zone: "utc" }).setZone("America/New_York");
-			const endDateTime = DateTime.fromISO(endTime, { zone: "utc" }).setZone("America/New_York");
+			const startDateTime = DateTime.fromISO(startTime, {
+				zone: "utc"
+			}).setZone("America/New_York");
+			const endDateTime =
+				endTime &&
+				DateTime.fromISO(endTime, { zone: "utc" }).setZone(
+					"America/New_York"
+				);
 
-			const formattedStartTime = startDateTime.toFormat("hh:mm a");
-			const formattedEndTime = endDateTime.toFormat("hh:mm a");
-
-			const time = `${formattedStartTime} - ${formattedEndTime}`;
-
-			const price = event.event.ticket_cost || event.event.filters.event_cost?.[0].name || "Not Provided";
+			const price =
+				event.event.ticket_cost ||
+				event.event.filters.event_cost?.[0].name ||
+				"Not Provided";
 
 			const url = event.event.localist_url;
 
 			const imageURL = event.event.photo_url;
 
 			await pool.query(
-				"INSERT INTO events (title, location, time, price, source, imageURL) VALUES ($1, $2, $3, $4, $5, $6)",
-				[title, location, time, price, url, imageURL]
+				"INSERT INTO events (title, location, time, price, source, imageURL, startTime, endTime) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
+				[
+					title,
+					location,
+					"time",
+					price,
+					url,
+					imageURL,
+					startDateTime,
+					endDateTime
+				]
 			);
 		}
 
